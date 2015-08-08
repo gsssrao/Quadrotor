1380a1db7eac2e54a815dcd7c22ced2f03cfa3c9
Srinivas <sairao1996@gmail.com>
2015-08-08 16:58:20 +0530

    Added other files

---
+++ PID Control/PID_Control.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+class State:
+    def __init__(self):
+        self.position = np.zeros((3,1))
+        self.velocity = np.zeros((3,1))
+
+class UserCode:
+    def __init__(self):
+        # TODO: tune gains
+    
+        # xy control gains
+        Kp_xy = 1.5 # xy proportional
+        Kd_xy = 0.6 # xy differential
+        
+        # height control gains
+        Kp_z  = 3.0 # z proportional
+        Kd_z  = 1.0 # z differential
+        
+        self.Kp = np.array([[Kp_xy, Kp_xy, Kp_z]]).T
+        self.Kd = np.array([[Kd_xy, Kd_xy, Kd_z]]).T
+    
+    def compute_control_command(self, t, dt, state, state_desired):
+        '''
+        :param t: time since simulation start
+        :param dt: time since last call to measurement_callback
+        :param state: State - current quadrotor position and velocity computed from noisy measurements
+        :param state_desired: State - desired quadrotor position and velocity
+        :return - xyz velocity control signal represented as 3x1 numpy array
+        '''
+        # plot current state and desired setpoint
+        self.plot(state.position, state_desired.position)
+        
+        # TODO: implement PID controller computing u from state and state_desired
+        u = np.zeros((3,1))
+        diff_state = state_desired.position-state.position
+        diff_velocity = state_desired.velocity-state.velocity
+        u = self.Kp*diff_state+self.Kd*diff_velocity
+        return u
+        
+    def plot(self, position, position_desired):
+        from plot import plot
+        plot("x", position[0])
+        plot("x_des", position_desired[0])
+        plot("y", position[1])
+        plot("y_des", position_desired[1])
+        plot("z", position[2])
+        plot("z_des", position_desired[2])
+        
